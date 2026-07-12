4ba0e7be2678c17e1a7b252f8613048ca16497b2
Pratik Pisudde <pratik.pisudde.005@gmail.com>
2026-07-12 23:02:05 +0530

    feat: setup clerk for auth & daisy UI for UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,12 +23,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+  return (<ClerkProvider>
+
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+  </ClerkProvider>
+
   );
 }
